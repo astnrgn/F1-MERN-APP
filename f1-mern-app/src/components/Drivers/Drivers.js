427fc8b4341d452c5170a36badfd46b10cbd0bed
Austin Michael Regan <austinmregan@gmail.com>
2019-08-13 16:53:59 -0400

    added home files

---
+++ f1-mern-app/src/components/Drivers/Drivers.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
+import "./Drivers.css";
+
+class Drivers extends Component {
+  render() {
+    return <div>HI</div>;
+  }
+}
+
+export default Drivers;
