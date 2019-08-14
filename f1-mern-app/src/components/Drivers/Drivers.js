85c4bce391e7a8bce568e35cb21d2239bd34bb59
Austin Michael Regan <austinmregan@gmail.com>
2019-08-14 11:51:44 -0400

    edited driver css

--- f1-mern-app/src/components/Drivers/Drivers.js
+++ f1-mern-app/src/components/Drivers/Drivers.js
@@ -5,43 +5,41 @@ import "./Drivers.css";
 class Drivers extends Component {
   render() {
     let drivers = this.props.driverData.map((driver, index) => {
-      if (index >= 6) {
-        return (
-          <div
-            className="driverDivs"
-            key={index}
-            style={{
-              backgroundImage: `url(${driver.image})`,
-              backgroundPosition: "center",
-              backgroundSize: "250px 250px",
-              backgroundRepeat: "no-repeat",
-              position: "relative",
-              textAlign: "center"
-            }}
-          >
-            <div className="textContainer">
-              <div className="text">
-                <p>
-                  <span className="description">Name: </span>
-                  {driver.name}
-                </p>
-                <p>
-                  <span className="description">Birthplace: </span>
-                  {driver.birthplace}
-                </p>
-                <p>
-                  <span className="description">Team: </span>
-                  {driver.team}
-                </p>
-                <p>
-                  <span className="description">Rank: </span>
-                  {driver.rank}
-                </p>
-              </div>
+      return (
+        <div
+          className="driverDivs"
+          key={index}
+          style={{
+            backgroundImage: `url(${driver.image})`,
+            backgroundPosition: "center",
+            backgroundSize: "250px 250px",
+            backgroundRepeat: "no-repeat",
+            position: "relative",
+            textAlign: "center"
+          }}
+        >
+          <div className="textContainer">
+            <div className="text">
+              <p>
+                <span className="description">Name: </span>
+                {driver.name}
+              </p>
+              <p>
+                <span className="description">Birthplace: </span>
+                {driver.birthplace}
+              </p>
+              <p>
+                <span className="description">Team: </span>
+                {driver.team}
+              </p>
+              <p>
+                <span className="description">Rank: </span>
+                {driver.rank}
+              </p>
             </div>
           </div>
-        );
-      }
+        </div>
+      );
     });
     return (
       <div>
