02fb7715442721818f723d4194deb406fc5043dc
Austin Michael Regan <austinmregan@gmail.com>
2019-08-15 15:05:05 -0400

    updated index.js with redux

--- f1-mern-app/src/index.js
+++ f1-mern-app/src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import * as serviceWorker from "./serviceWorker";
 
+import store from "./store";
+import { Provider } from "react-redux";
+store.subscribe(() => console.log(store.getState()));
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
